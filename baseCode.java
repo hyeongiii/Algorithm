cbb34862a65028b069bf924e7359f62ef2a37cbf
hyeongiii <hyeongiii0811@gmail.com>
2023-10-10 20:25:25 +0900

    기본 : 자바 알고리즘 기본코드

---
+++ baseCode.java
@@ -0,0 +1,60 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static FastReader scan;
+    public static StringBuilder sb;
+
+    public static int n, m;
+
+    static void input() {
+        n = scan.nextInt();
+        m = scan.nextInt();
+    }
+
+    public static void main(String[] args) {
+        input();
+    }
+
+    static class FastReader {
+        BufferedReader br;
+        StringTokenizer st;
+
+        public FastReader() {
+            br = new BufferedReader(new InputStreamReader(System.in));
+        }
+
+        public FastReader(String s) throws FileNotFoundException {
+            br = new BufferedReader(new FileReader(new File(s)));
+        }
+
+        String next() {
+            while (st == null || !st.hasMoreElements()) {
+                try {
+                    st = new StringTokenizer(br.readLine());
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+            return st.nextToken();
+        }
+
+        int nextInt() {
+            return Integer.parseInt(next());
+        }
+
+        double nextDouble() {
+            return Double.parseDouble(next());
+        }
+
+        String nextLine() {
+            String str = "";
+            try {
+                str = br.readLine();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            return str;
+        }
+    }
+}
